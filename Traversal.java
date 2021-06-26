d74868c59d457f36250f79d96d9bc17d656f7875
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-06-26 17:26:28 +0800

    二叉树

--- Traversal.java
+++ Traversal.java
@@ -143,6 +143,57 @@ public class Traversal {
         }
         return ret||isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
     }
+    //二叉树深度
+    public int maxDepth(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }
+        if(root.left==null&&root.right==null) {
+            return 1;
+        }
+        int leftDepth=maxDepth(root.left);
+        int rightDepth=maxDepth(root.right);
+        return 1+(leftDepth>rightDepth?leftDepth:rightDepth);
+
+    }
+    //平衡二叉树
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) {
+            return true;
+        }
+        if(root.left==null&&root.right==null) {
+            return true;
+        }
+        int leftDepth=maxDepth(root.left);
+        int rightDepth=maxDepth(root.right);
+        if(leftDepth-rightDepth>1||rightDepth-leftDepth>1) {
+            return false;
+        }
+        return isBalanced(root.left)&&isBalanced(root.right);
+
+    }
+    //对称二叉树
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) {
+            return true;
+        }
+        return isMirror(root.left,root.right);
+
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if(t1==null&&t2==null) {
+            return true;
+        }
+        if(t1==null||t2==null) {
+            return false;
+        }
+        if(t1.val!=t2.val) {
+            return false;
+        }
+        return isMirror(t1.left,t2.right)&&isMirror(t1.right,t2.left);
+    }
+
     public static void main(String[] args) {
         Traversal traversal=new Traversal();
         TreeNode root=traversal.createTree();

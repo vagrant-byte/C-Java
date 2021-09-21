9704607a4fd360d204183c1a55927b6c5d4c6927
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-09-21 15:55:40 +0800

    用户信息

--- User.java
+++ User.java
@@ -1,18 +1,49 @@
-package user;
+package jdbc;
 
-import book.BookList;
-import operation.IOperation;
+import jdbc.operation.IOperation;
 
+//针对isAdmin 并不是使用boolean变量来区分 而是使用两个不同的子类
+//原因是，管理员和普通用户支持的方法是不一样的
 abstract public class User {
-    protected String name;
+    private int userid;
+    private String username;
+    private String password;
+    //包含一个数组，数组里边就是该用户支持那些操作
+    //针对普通用户和管理员分别设置不同的操作
+    protected IOperation[] operations;
+    //普通用户和管理员看到的菜单
+    abstract public int menu();
 
-    public User(String name) {
-        this.name=name;
+    public int getUserid() {
+        return userid;
     }
 
-    protected IOperation[] operations;
-    abstract public  int menu();
-    public void doOperation(int choice, BookList bookList) {
-        operations[choice].work(bookList);
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userid=" + userid +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }

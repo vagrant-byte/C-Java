fa1b0b38f7d601d4357ee4827a922065d7e3395d
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-09-21 15:56:48 +0800

    普通用户

--- NormalUser.java
+++ NormalUser.java
@@ -1,15 +1,15 @@
-package user;
+package jdbc;
 
-import book.BookList;
-import operation.*;
+import jdbc.operation.*;
 
 import java.util.Scanner;
 
 public class NormalUser extends User {
-    public NormalUser(String name) {
-        super(name);
+    public NormalUser() {
+        //初始化operations
         this.operations=new IOperation[] {
                 new ExitOperation(),
+                new DisplayOperation(),
                 new FindOperation(),
                 new BorrowOperation(),
                 new ReturnOperation()
@@ -18,17 +18,18 @@ public class NormalUser extends User {
 
     @Override
     public int menu() {
-        System.out.println("================================");
-        System.out.println("hello"+ this.name+"欢迎使用图书管理系统");
-        System.out.println("1.查找图书");
-        System.out.println("2.借阅图书");
-        System.out.println("3.归还图书");
+        //打印普通用户菜单
+        System.out.println("====================");
+        System.out.println("欢饮您，"+this.getUsername()+"!");
+        System.out.println("1.查看书籍列表");
+        System.out.println("2.查找指定书籍");
+        System.out.println("3.借阅书籍");
+        System.out.println("4.归还书籍");
         System.out.println("0.退出系统");
-        System.out.println("================================");
-        System.out.println("请输入你的选择");
+        System.out.println("====================");
+        System.out.println("请输入选项：");
         Scanner scanner=new Scanner(System.in);
         int choice=scanner.nextInt();
         return choice;
     }
-
 }
